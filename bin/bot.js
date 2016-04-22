bdf15543fb373830f32b682d9030a67d303c0716
Cameron Riera <criera@zonoff.com>
2016-04-22 16:31:17 -0400

    Refactored ArcherBot to handle posting to channels or groups

--- bin/bot.js
+++ bin/bot.js
@@ -18,7 +18,7 @@ var ArcherBot = require('../src/archerBot');
  */
 var token = process.env.BOT_API_KEY || require('../token');
 //var dbPath = process.env.BOT_DB_PATH;
-var name = process.env.BOT_NAME || '@archer';
+var name = process.env.BOT_NAME || 'archer';
 
 var archerbot = new ArcherBot({
   token: token,
--- src/archerBot.js
+++ src/archerBot.js
@@ -7,7 +7,7 @@ var responses = require('../data/responses')
 var Bot = require('slackbots');
 
 const PHRASING_TRIGGER_POINT_VAL = 12;
-var CANT_WONT_REGEXP = new RegExp(/(i|we)\s(cant|can\'t)/, 'ig');
+var CANT_WONT_REGEXP = (/(i|we)\s(cant|can't)/ig);
 var joinedUser = '';
 
 var ArcherBot = function Constructor(settings) {
@@ -22,17 +22,14 @@ var ArcherBot = function Constructor(settings) {
 // inherits methods and properties from the Bot constructor
 util.inherits(ArcherBot, Bot);
 
-
 /**
  * Run the bot
  * @public
  */
 ArcherBot.prototype.run = function () {
   ArcherBot.super_.call(this, this.settings);
-
   this.on('start', this._onStart);
   this.on('message', this._onMessage);
-  this.on('message', this._onChannelJoin);
 };
 
 /**
@@ -41,9 +38,10 @@ ArcherBot.prototype.run = function () {
  */
 ArcherBot.prototype._onStart = function () {
   this._loadBotUser();
+  console.log('archer bot: ' + this.user);
   //this._connectDb();
   //this._firstRunCheck();
-  this._welcomeMessage();
+  //this._welcomeMessage();
 };
 
 /**
@@ -52,53 +50,52 @@ ArcherBot.prototype._onStart = function () {
  * @private
  */
 ArcherBot.prototype._onMessage = function (message) {
-  if (this._isChatMessage(message) && this._isChannelConversation(message) && !this._isFromArcherBot(message)) {
-    if(this._isReplyFromJustJoinedUser(message)){
-
+  console.log(message);
+  if (this._isChatMessage(message) && this._isChannelOrGroupConversation(message) && !this._isFromArcherBot(message)) {
+    //if(this._isReplyFromJustJoinedUser(message)){
+    //  if((/wha+t/ig).test(message.text)){
+    //    this._postMessage(message, pickRandom(responses.dangerZone));
+    //  }
+    //  joinedUser = '';
+    //}
+    if (this._isMentioningArcher(message)) {
+      this._postMessage(message, pickRandom(responses.random));
     }
-    else if(this._isMentioningArcher(message)){
-      this._replyWithRandomResponse(message);
+    else if (this._isTriggerCantWont(message)) {
+      this._postMessage(message, 'Can\'t or won\'t?');
     }
-    else if(this._isTriggerCantWont(message)){
-      this._replyWithCantOrWont(message);
+    else if (this._isTriggerPhrasingResponse(message)) {
+      this._postMessage(message, pickRandom(responses.phrasing));
     }
-    else if(this._isTriggerPhrasingResponse(message)) {
-      this._replyWithRandomPhrasing(message);
-    }
-  } else if (this._isChannelJoin(message)){
-    var channelName = this._getChannelNameFromMessageText(message);
-    this._replyWithDangerZoneDiatribe(message, channelName);
   }
+  // else if (this._isChannelJoin(message)){
+  //  var channelName = this._getChannelNameFromMessageText(message);
+  //  this._replyWithDangerZoneDiatribe(message, channelName);
+  //}
 };
 
+/**
+ * Checks whether the message text contains enough innuendo to trigger a 'PHRASING' response
+ * @param message
+ * @returns {boolean}
+ * @private
+ */
 ArcherBot.prototype._isTriggerPhrasingResponse = function (message) {
   return phrasing.phrasingScore(message.text) >= PHRASING_TRIGGER_POINT_VAL;
 };
 
-ArcherBot.prototype._replyWithRandomPhrasing = function (originalMessage) {
-  var self = this;
-  var response = pickRandom(responses.phrasing);
-  var channel = self._getChannelById(originalMessage.channel);
-  self.postMessageToChannel(channel.name, response, {as_user: true});
-};
-
+/**
+ * Checks whether the message text contains value for appropriate 'Cant or wont?' response
+ * @param message
+ * @returns {boolean}
+ * @private
+ */
 ArcherBot.prototype._isTriggerCantWont = function (message) {
   return CANT_WONT_REGEXP.test(message.text);
 };
 
-ArcherBot.prototype._replyWithCantOrWont = function (originalMessage) {
-  var self = this;
-  var channel = self._getChannelById(originalMessage.channel);
-  self.postMessageToChannel(channel.name, 'Can\'t or won\'t?', {as_user: true});
-};
-
 ArcherBot.prototype._isReplyFromJustJoinedUser = function (message) {
-  var self = this;
-  if((/wha+t/ig).test(message.text)){
-    var channel = self._getChannelById(originalMessage.channel);
-    self.postMessageToChannel(channel.name, responses.dangerZone, {as_user: true});
-  }
-  joinedUser = '';
+  return message.user === joinedUser.name;
 };
 
 ArcherBot.prototype._replyWithDangerZoneDiatribe = function (originalMessage, channelName) {
@@ -112,74 +109,6 @@ ArcherBot.prototype._replyWithDangerZoneDiatribe = function (originalMessage, ch
     }, 2000);
   }
 };
-
-function repeatName(name, replyCount){
-  var response = name;
-  switch(replyCount) {
-    case 0:
-      response = name.concat('.');
-      break;
-    case 1:
-      response = name.concat('!');
-      break;
-    case 2:
-      response = name.toUpperCase().concat('!');
-      break;
-    case 3:
-      response = yellName(name);
-      break;
-    default:
-      response = name;
-  }
-  return response;
-}
-
-function yellName(name) {
-  var VOWEL_REGEXP = /(a|e|i|o|u|y)/ig;
-
-  var lastVowelIndex;
-  var vowel;
-  for(var i = name.length-1; i > 0; i--){
-    if(VOWEL_REGEXP.test(name.charAt(i))){
-      lastVowelIndex = i;
-      vowel = name.charAt(i);
-      break;
-    }
-  }
-  var nameBegin = name.slice(0, lastVowelIndex);
-  var nameEnd = name.slice(lastVowelIndex, name.length);
-
-  name = nameBegin + vowel.repeat(5) + nameEnd + '!';
-  return name.toUpperCase();
-}
-
-
-
-/**
- * Replyes to a message with a random Joke
- * @param {object} originalMessage
- * @private
- */
-ArcherBot.prototype._replyWithRandomResponse = function (originalMessage) {
-  //var self = this;
-  //self.db.get('SELECT id, joke FROM jokes ORDER BY used ASC, RANDOM() LIMIT 1', function (err, record) {
-  //  if (err) {
-  //    return console.error('DATABASE ERROR:', err);
-  //  }
-  //
-  //  var channel = self._getChannelById(originalMessage.channel);
-  //  self.postMessageToChannel(channel.name, record.joke, {as_user: true});
-  //  self.db.run('UPDATE jokes SET used = used + 1 WHERE id = ?', record.id);
-  //});
-  var self = this;
-  var channel = self._getChannelById(originalMessage.channel);
-  self.postMessageToChannel(channel.name, pickRandom(responses.random), {as_user: true});
-};
-
-function pickRandom(list) {
-  var idx = Math.floor(Math.random() * list.length);
-  return list[idx];
-}
 
 /**
  * Loads the user object representing the bot
@@ -198,9 +127,12 @@ ArcherBot.prototype._loadBotUser = function () {
  * @private
  */
 ArcherBot.prototype._welcomeMessage = function () {
-  this.postMessageToChannel(this.channels[0].name, 'Was anyone looking for the worlds greatest secret agent?' +
-    '\n If not, just say my name`' + this.name + '` and I\'ll be there... or not. Its not like I\'m your servant like Woodhouse',
-    {as_user: true});
+  console.log('replying with welcome message to channel ' + this.channels[0].name);
+  console.log('replying with welcome message to group' + this.groups[0].name);
+  var response = 'Was anyone looking for the worlds greatest secret agent?' +
+      '\n If not, just say my name `' + this.name + '` and I\'ll be there... or not. Its not like I\'m your servant like Woodhouse';
+  this._postMessage(this.channels[0].name, response);
+  this._postMessage(this.groups[0].name, response);
 };
 
 /**
@@ -221,9 +153,47 @@ ArcherBot.prototype._isChatMessage = function (message) {
  */
 ArcherBot.prototype._isChannelConversation = function (message) {
   return typeof message.channel === 'string' &&
-    message.channel[0] === 'C'
-    ;
+    message.channel[0] === 'C';
 };
+
+/**
+ * Util function to check if a given real time message object is directed to a group
+ * @param message
+ * @returns {boolean}
+ * @private
+ */
+ArcherBot.prototype._isGroupConversation = function (message) {
+  return typeof message.channel === 'string' && message.channel[0] === 'G';
+};
+
+/**
+ * Util function to check if a given real time message object is directed to a group
+ * @param message
+ * @returns {boolean}
+ */
+ArcherBot.prototype._isChannelOrGroupConversation = function (message) {
+  return typeof message.channel === 'string' && this._isChannelConversation(message) ||
+      this._isGroupConversation(message);
+};
+
+
+/**
+ * Util function to post message either to channel or group
+ * @param originalMessage
+ * @param response
+ * @private
+ */
+ArcherBot.prototype._postMessage = function (originalMessage, response) {
+  if(this._isChannelConversation(originalMessage)){
+    var channel = this._getChannelById(originalMessage.channel);
+    this.postMessageToChannel(channel.name, response, {as_user: true});
+  }
+  if(this._isGroupConversation(originalMessage)){
+    var group = this._getGroupById(originalMessage.channel);
+    this.postMessageToGroup(group.name, response, {as_user: true});
+  }
+};
+
 
 /**
  * Util function to check if a given real time message object is because of a channel_join event
@@ -267,6 +237,7 @@ ArcherBot.prototype._getChannelNameFromMessageText = function (message) {
  */
 ArcherBot.prototype._isMentioningArcher = function (message) {
   return message.text.toLowerCase().indexOf('archer') > -1 ||
+      message.text.toLowerCase().indexOf('sterling') > -1 ||
     message.text.toLowerCase().indexOf(this.name) > -1;
 };
 
@@ -292,9 +263,77 @@ ArcherBot.prototype._getChannelById = function (channelId) {
   })[0];
 };
 
+ArcherBot.prototype._getGroupById = function (groupId) {
+  return this.groups.filter(function (item) {
+    return item.id === groupId;
+  })[0];
+};
 
+function repeatName(name, replyCount){
+  var response = name;
+  switch(replyCount) {
+    case 0:
+      response = name.concat('.');
+      break;
+    case 1:
+      response = name.concat('!');
+      break;
+    case 2:
+      response = name.toUpperCase().concat('!');
+      break;
+    case 3:
+      response = yellName(name);
+      break;
+    default:
+      response = name;
+  }
+  return response;
+}
+
+function pickRandom(list) {
+  var idx = Math.floor(Math.random() * list.length);
+  return list[idx];
+}
+
+function yellName(name) {
+  var VOWEL_REGEXP = /(a|e|i|o|u|y)/ig;
+
+  var lastVowelIndex;
+  var vowel;
+  for(var i = name.length-1; i > 0; i--){
+    if(VOWEL_REGEXP.test(name.charAt(i))){
+      lastVowelIndex = i;
+      vowel = name.charAt(i);
+      break;
+    }
+  }
+  var nameBegin = name.slice(0, lastVowelIndex);
+  var nameEnd = name.slice(lastVowelIndex, name.length);
+
+  name = nameBegin + vowel.repeat(5) + nameEnd + '!';
+  return name.toUpperCase();
+}
 
 //////////////////////////////// DB Implementation \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+/**
+ * Replyes to a message with a random Joke
+ * @param {object} originalMessage
+ * @private
+ */
+//ArcherBot.prototype._replyWithRandomResponse = function (originalMessage) {
+//  var self = this;
+//  self.db.get('SELECT id, joke FROM jokes ORDER BY used ASC, RANDOM() LIMIT 1', function (err, record) {
+//    if (err) {
+//      return console.error('DATABASE ERROR:', err);
+//    }
+//
+//    var channel = self._getChannelById(originalMessage.channel);
+//    self.postMessageToChannel(channel.name, record.joke, {as_user: true});
+//    self.db.run('UPDATE jokes SET used = used + 1 WHERE id = ?', record.id);
+//  });
+//};
+
 
 //var SQLite = require('sqlite3').verbose();
 //var path = require('path');
